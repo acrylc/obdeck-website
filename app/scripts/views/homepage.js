7e64ab9e6a8d6ca8a81ca0ecfe417726ea29c41f
Maya Kreidieh <mayakreidieh@gmail.com>
2013-07-08 00:44:00 +0300

    Create template for full issuepage, page can be dynamically created from any issue object

--- app/scripts/views/homepage.js
+++ app/scripts/views/homepage.js
@@ -3,8 +3,9 @@ var app = app || {
 	Views: {},
 	Collections: {},
 	Routers: {},
-	Defaults: {},
+	Defaults: {}
 };
+
 
 // Load the application once the DOM is ready, using `jQuery.ready`:
 $(function(){
@@ -20,33 +21,96 @@ $(function(){
 		el: "#content",
 
 		template: _.template( $('#home-view-template').html() ),
+		kptemplate: _.template( $('#keyplayer-template').html() ),
 
 		// Listen to navbar, update content accordingly
 		events: {
 		},
 
 		initialize: function() {
-			console.log(' in initalize ');
-			this.render();
-			this.visual = new app.Views.List({"issue":"test"});
-			console.log("in main");
-			$('#my-timeline').blurjs({
-			source: '#my-timeline',
-			radius: 12,
-			overlay: 'rgba(255,255,255,0.4)'
-			});
-			$('#navbar').show();
-			this.navIsFixed = false;
 
-			var that = this;
+
+			// temporary json object containing all the issue parameters to 
+			// dynamically create an issue page
+			 app.issue = {
+				overview : " While they realize they will not be able to reverse the 17-month extension of parliament’s mandate that became effective today, protestors are determined to stay at the southern entrance of Nejmeh Square to make their voices heard. Exactly how long they’ll remain, however, is still unclear. As the call to prayer reverberated throughout downtown and a steady stream of traffic circumnavigated a small cluster of tents and people positioned in the middle of the street, Naji Raji sat and talked with fellow demonstrators. Raji, a founder of Save Beirut Heritage, said he came down to protest the inaction of successive legislatures on an issue dear to his heart. “My cause is Save Beirut Heritage,” he said. “There’s been a law [to protect old buildings] put in parliament’s drawers for [six] years now.” Asked what he hopes to accomplish given that parliament’s term extension has now gone into effect, Raji said he wanted “to make some noise.” Lebanon’s last parliamentary elections took place in 2009, and the current legislature’s term was set to expire on June 20. For most of the past year, the country’s political parties have been bickering over a new electoral law to replace the one that governed the last polls. During that time, politicians from across the spectrum confided to NOW that they believed the elections would be postponed.",
+				timeline_doc : "https://docs.google.com/spreadsheet/pub?key=0AvsGYBn6aGTpdFlVWFB6S2JmczBON3gtM1J2SHhpVlE&output=html",
+				heroimage : "../images/hala.jpg",
+				keyplayers: [
+					{	name : "Take back parliament", 
+						description : "A short description describing this groups relation to the issue, and why to contact. ", 
+						logo : "images/tbp.jpg",
+						contact : {fb : "fb.com/vote2013"} 
+					},
+					{	name : "من أجل الجمهورية", 
+						description : "A short description describing this groups relation to the issue, and why to contact. ", 
+						logo : "images/mag.jpg",
+						contact :  {fb : "fb.com/vote2013"} 
+					},
+					{	name : "Lebanese parliament", 
+						description : "A short description describing this groups relation to the issue, and why to contact. ", 
+						logo : "images/lp.svg",
+						contact : {} 
+					}
+				]
+			};
+
+			 app.issue = {
+				overview : " Yesterday night, Nadine Moawad, spokesperson for Nasawiya, posted on Facebook “Urgent. The shabbee7a of Nadim Gemayel just carried machine guns in our faces in Nasawiya. We are now locked in.” Later we found out that apparently the bodyguards entered the Nasawiya HQ in Mar Mikhael and demanded that the feminists stop taking pictures. Taking pictures? Yes, Nasawiya was having a farewell party and pictures tend to be part of a party. The ‘problem’, according to the thugs, is that our dear Nadim Gemayel was having a dinner nearby and apparently has a reputation of being paranoid. When the feminists refused (and rightly so), the thugs pointed machine guns at an activist and threatened to shoot saying they had “permission to do so”. The feminists got scared and locked themselves in, waiting for the police to intervene, which they eventually did. Part of the confrontation was filmed and uploaded on Facebook.",
+				timeline_doc : "https://docs.google.com/spreadsheet/pub?key=0AvsGYBn6aGTpdDc5YlVFeVdzek02VjlsQUdXeUFra1E&output=html",
+				heroimage : "../images/pat.jpg",
+				keyplayers: [
+					{	name : "Nadim Gemayel", 
+						description : "A short description describing this groups relation to the issue, and why to contact. ", 
+						logo : "images/nadimgemayel.jpg",
+						contact :  {fb : "fb.com/vote2013"} 
+					},
+					{	name : "Take back parliament", 
+						description : "A short description describing this groups relation to the issue, and why to contact. ", 
+						logo : "images/tbp.jpg",
+						contact : {fb : "fb.com/vote2013"} 
+					}
+				]
+			};
+
+			this.render();
+			this.initEventTimeline();
+			this.initNavbar();
+			this.initSocialTimeline();
+
+			 for(var i=0;i<app.issue.keyplayers.length;i++){
+ 				 $('#players').append( this.kptemplate( app.issue.keyplayers[i]) );
+			}
+
+		},
+
+		initEventTimeline: function(){
+
+			// Set background image to hero unit
+			$('#event-timeline').css({'background-image' : ' url('+ app.issue.heroimage +')'});
+				
+			// Blur hero unit
+			$('#event-timeline').blurjs({
+				source: '#event-timeline',
+				radius: 12,
+				overlay: 'rgba(255,255,255,0.4)'
+			});
+			
+			// Create main timeline using TimelineJS
 			createStoryJS({
 				type:       'timeline',
 				width:      '100%',
 				height:     '550',
-				source:     'https://docs.google.com/spreadsheet/pub?key=0AvsGYBn6aGTpdFlVWFB6S2JmczBON3gtM1J2SHhpVlE&output=html',
-				embed_id:   'my-timeline',
+				source:     app.issue.timeline_doc,
+				embed_id:   'event-timeline',
 				css: 'timeline-style/timeline.css'
 			});
+		},
+
+		initNavbar: function(){
+			var that = this;
+			$('#navbar').show();
+			this.navIsFixed = false;
 
 			 $(window).scroll(function(){
 			// get the height of #wrap
@@ -64,19 +128,23 @@ $(function(){
 						$('#navbar').css('display','block');
 					});
 				}
-
 			});
+		},
+
+		initSocialTimeline: function(){
+			this.visual = new app.Views.List({"issue":"test"});
 
 		},
 
 		render: function(){
 			$(this.el).empty();
-			$(this.el).html((this.template()));
+			$(this.el).html((this.template({'overview':app.issue.overview})));
 			return this;
 		},
 
+
+
 		reset: function(){
-			////console.log('resetting');			
 			this.visual.destroy();
 		},
 
@@ -87,154 +155,7 @@ $(function(){
 			//Clear DOM
 			this.remove();  
 			Backbone.View.prototype.remove.call(this);
-		},
-
-		displayStories: function(t){
-					        var that = this;
-
-			if (t.toElement.id == 'map'){
-				document.getElementById('map').id='list';
-				$('.display').html("List");
-
-				$('.filter').fadeOut('fast').animate({
-		            'margin-right': '-100px'
-		            }, {duration: 'fast', queue: false});
-				$('#trending').fadeOut('fast').animate({
-		            'margin-left': '-100px'
-		            }, {duration: 'fast', queue: false});
-
-				$('#recent').fadeOut('fast').animate({
-		            'margin-left': '-100px'
-		            }, {duration: 'fast', queue: false, complete:function() {
-		            	that.displayMap();
-		        }
-		    });
-
-
-			} else {
-				this.display = 'list';
-				document.getElementById('list').id='map';
-				$('.display').html("Map");
-
-				// animations
-				$('.filter').fadeIn('fast').animate({
-		            'margin-right': '0px'
-		            }, {duration: 'fast', queue: false}, function() {
-		            // Animation complete.
-		        });
-				$('#trending').fadeIn('fast').animate({
-		            'margin-left': '0px'
-		            }, {duration: 'fast', queue: false}, function() {
-		            // Animation complete.
-		        });
-				$('#recent').fadeIn('fast').animate({
-		            'margin-left': '0px'
-		            }, {duration: 'fast', queue: false}, function() {
-		            // Animation complete.
-		        });
-
-				this.displayList();
-			} 
-		},
-
-		
-		displayStatistics: function(){
-			this.numStories = 0;
-			this.talking = [];
-			this.tags = [];
-			var that = this;
-			var s = {};
-			this.count = 0;
-			stor=[];
-
-			var ref5 = new Firebase('https://lebelec.firebaseio.com/hashtags/' + this.options.issue + '/trending');
-
-
-        	ref5.once('value', function (snapshot) {
-        		s = snapshot;
-        		that.numStories = snapshot.numChildren();
-        		//console.log("NUM STORIES" +that.numStories);
-
- 				ref5.on('child_added', function (snapshot, prevName) {
-                if (snapshot.val() === null) {
-                } else {
-                	//console.log(that.talking.length);
-                    var storyref = snapshot.val();
-                    var story = new Firebase('https://lebelec.firebaseio.com/stories/' + snapshot.val());
-                    story.on('value', function (snapshot) {
-                        if (snapshot.val() === null) {
-                        } else {
-                           var s = snapshot.val();
-                           stor[stor.length]=s;
-                        }
-						//console.log(that.count);
-						//console.log(that.numStories);
-						that.count = that.count+1;
-						//console.log('NUM STORIES WE HAVE ' +stor.length);
-						if (stor.length == that.numStories){
-
-								//console.log(stor.length);
-								for(var k=0;k<stor.length;k++){
-									s = stor[k];
-									//console.log(k);
-									//console.log(stor[k]);
-									var ret =  _(that.talking).find(function(t){
-	                            	return t.user==s.user;
-	                            });
-	                            if (ret==undefined){
-	                            	that.talking.push({'user':s.user, 'count':0});
-	                            } else {
-	                            	ret.count = ret.count+1;
-	                            }
-	                            ret = undefined;
-
-	                            for(var i=0;i<s.category.length;i++){
-									var ret =  _(that.tags).find(function(t){
-		                            	return t.tag==s.category[i];
-		                            });
-		                            if (ret==undefined){
-		                            	that.tags.push({'tag':s.category[i], 'count':0});
-		                            } else {
-		                            	ret.count = ret.count+1;
-		                            }
-	        					}
-								}
-								//console.log("LENGTH" + that.talking.length);
-								$('#talkingaboutvalue').html(that.talking.length);
-								$('#numstoriesvalue').html(that.numStories);
-								
-								for(var i=1;i<=10;i++){
-									if (that.tags[i]!=undefined){
-										$('#tags').append("<li> " + i + " :  <strong> "+that.tags[i].tag+" </strong> </li>");
-									}
-								}
-							}
-						});
-                }
-            });
-
-
-            });
- 
-		},
-
-		displayMap: function(){
-			this.reset();
-			console.log('displaying map');
-			$('.more').css({'display':'none'});
-			 $(this.el).children('#d').append("<div id='dynamic-content'></div>");
-
-			 this.visual = new app.Views.Map({"issue":this.options.issue});
-		},
-
-		displayList: function(){
-			 this.reset();
-			 			$('.more').css({'display':'block'});
-
-			  $(this.el).children('#d').append("<div id='dynamic-content'></div>");
-
-			this.visual = new app.Views.List({"issue":this.options.issue});
-		}		
+		}	
 
 	});
 
